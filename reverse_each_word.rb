4dad384c143acfe7b7200748a2ff72d0a1f33080
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-02 18:40:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,7 @@
-def reverse_each_word(word)
-  array = word.split(" ")
+def reverse_each_word(sentence)
+  array = sentence.split
   new_array = []
   array.each do |word|
-    new_array << word.reverse
-  end
-  new_array.join(" ")
-end
-
-def reverse_each_word_collect(word)
-  array = word.split(" ")
-  new_array = []
-  array.collect do |word|
     new_array << word.reverse
   end
   new_array.join(" ")
